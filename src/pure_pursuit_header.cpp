7225dfef8a651fd59c5fa1bcbc5866fce42b6d6a
NikkukkiN <nikku_alatur@yahoo.de>
2016-12-06 17:23:25 +0100

    Lol

--- src/pure_pursuit_header.cpp
+++ src/pure_pursuit_header.cpp
@@ -15,7 +15,7 @@ void PurePursuit::safeThePath(const nav_msgs::Path::ConstPtr& subscribed)
 	path_=*subscribed;
 	this->calculateU();
 	this->publishU();
-	
+
 	}
 
 void PurePursuit::sts(const arc_msgs::State::ConstPtr& subscribed)
@@ -27,15 +27,15 @@ void PurePursuit::sts(const arc_msgs::State::ConstPtr& subscribed)
 
 
 
-float* PurePursuit::pathInfo(float where)			//hier interpoliert und info at "where" (derivative ecc) in ARRAY zurück 
-	{							//provisorisch, bevor mit Pfadpunkten gearbeiteet wird	
+float* PurePursuit::pathInfo(float where)			//hier interpoliert und info at "where" (derivative ecc) in ARRAY zurück
+	{							//provisorisch, bevor mit Pfadpunkten gearbeiteet wird
 	float R=10;						//array:{x_coordinate,y_coordinate,..}
-	std::vector<float> x_pfad(2);	
+	std::vector<float> x_pfad(2);
 	x_pfad[0]=R*cos(where);
 	x_pfad[1]=R*sin(where);
 	float* pointer=&x_pfad[0] ;
 	return pointer;
-	}		
+	}
 
 ackermann_msgs::AckermannDrive PurePursuit::getU()		//u wird zurückgegeben
 	{
@@ -57,7 +57,7 @@ void PurePursuit::calculateU()					//Schritte um u zu berechnen (Reglerspezyfisc
 	else
 	{
 	float v=10.0;
-	float L=3.0;					
+	float L=3.0;
 	float v_abs=sqrt(pow(state_.pose_diff.twist.linear.x,2)+pow(state_.pose_diff.twist.linear.x,2));
 	float l=(k_)*v_abs;
 	float j=findReference(l);
@@ -68,18 +68,18 @@ void PurePursuit::calculateU()					//Schritte um u zu berechnen (Reglerspezyfisc
 
 	float ox=state_.pose.pose.orientation.x;		//Transformation von Quaternion zu Euler
 	float oy=state_.pose.pose.orientation.y;
-	float oz=state_.pose.pose.orientation.z;it::s
+	float oz=state_.pose.pose.orientation.z;
 	float ow=state_.pose.pose.orientation.w;
 	const Eigen::Vector4d quat(ox, oy, oz, ow);
 	geometry_msgs::Vector3 eul;
-	eul=arc_tools::transformEulerQuaternion(quat);
+	eul=transformEulerQuaternion(quat);
 	float theta2=eul.z;		// muss noch ent-quaternionisiert werden
 	float alpha=theta1-theta2;
-		//float x_j=path.poses[10].pose.position.x;				
-	
-	u_.speed=v;									
+		//float x_j=path.poses[10].pose.position.x;
+
+	u_.speed=v;
 	u_.steering_angle=atan2(2*L*sin(alpha),l);;
-	}	
+	}
 	}
 
 float PurePursuit::findReference(float l)
@@ -88,7 +88,7 @@ float PurePursuit::findReference(float l)
 	int j;
 	for(float i=state_.current_arrayposition; i<(state_.current_arrayposition+3); i=i+0.1)	//je nach Pfad grenzen und schritte
 												//einstellen
-		{	
+		{
 		float x_pfad=pathInfo(i)[0];
 		float y_pfad=pathInfo(i)[1];
 		float x_now=state_.pose.pose.position.x;
@@ -98,19 +98,19 @@ float PurePursuit::findReference(float l)
 			{
 			e=d;
 			j=i;
-			}	
+			}
 		}
 	state_.current_arrayposition=j;
 	return j;
 	}
 
 void PurePursuit::publishU()
-	{	
+	{
 	pub_stellgroessen_ = n_->advertise<ackermann_msgs::AckermannDrive>("stellgroessen", 1000);
 	pub_stellgroessen_.publish(u_);
 	}
 
-float* PurePursuit::projectOnPath()					//gibt i.a. nicht Punkt 
+float* PurePursuit::projectOnPath()					//gibt i.a. nicht Punkt
 									//aus path zurück, sondern zwischen zwei...
 	{
 	float x_projected[3];						//Eintrag 1,2 koordinaten, eintrag 3 currentarrayposition
@@ -122,12 +122,12 @@ float* PurePursuit::projectOnPath()					//gibt i.a. nicht Punkt
 	float ow=state_.pose.pose.orientation.w;
 	const Eigen::Vector4d quat(ox, oy, oz, ow);
 	geometry_msgs::Vector3 eul;
-	eul=arc_tools::transformEulerQuaternion(quat);
+	eul=transformEulerQuaternion(quat);
 	float theta=eul.z;
 
 	float x_now=state_.pose.pose.position.x;
 	float y_now=state_.pose.pose.position.y;
-	for(int i=0;i<(sizeof(path_.poses)/sizeof(path_.poses[0]));i++)		//schaut alle LINEAREN Interpolationen 
+	for(int i=0;i<(sizeof(path_.poses)/sizeof(path_.poses[0]));i++)		//schaut alle LINEAREN Interpolationen
 										//zw punktepaaren durch
 		{
 		float x_j=path_.poses[i+1].pose.position.x;
@@ -138,7 +138,7 @@ float* PurePursuit::projectOnPath()					//gibt i.a. nicht Punkt
 		float lambda=(tan(theta)*(y_i-y_now)-(x_i-x_now))/((x_j-x_i)-tan(theta)*(y_j-y_i));	//analytisch
 		if(0<=lambda&&lambda<=1)
 			{
-			x_projected[0]=x_i+lambda*(x_j-x_i);			
+			x_projected[0]=x_i+lambda*(x_j-x_i);
 			x_projected[1]=y_i+lambda*(y_j-y_i);
 			x_projected[2]=i;
 			x_projected_p=&x_projected[0];
@@ -148,4 +148,3 @@ float* PurePursuit::projectOnPath()					//gibt i.a. nicht Punkt
 	if(&x_projected_p==NULL){std::cout<<"Fehler, senkrecht zu Fahrzeug liegt kein Pfad";}
 	return x_projected_p;						//Sollte eine fehlermeldung geben
 	}
-
