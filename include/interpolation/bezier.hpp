7b958857b4fe54d701f20c0ef7c697b4ccfc199b
NikkukkiN <nikku_alatur@yahoo.de>
2017-02-17 13:19:52 +0100

    BezierInterpolation added but not functional yet

--- include/interpolation/bezier.hpp
+++ include/interpolation/bezier.hpp
@@ -1,5 +1,62 @@
-/* This class should have atleast the following methods.
-At the beginning, cubicbezier will be hard coded. With the neares point being P1.
-1. Hand over n points to interpolate from weher it is clear which one is the current point.
-2.
-*/
+#pragma once
+#include "math.h"
+#include "nav_msgs/Path.h"
+#include <iostream>
+
+class BezierCurve{
+
+  public:
+    // Default constructor.
+    BezierCurve();
+    // Useful constructor for pure pursuit.
+    BezierCurve(nav_msgs::Path path, int numb_ctr_points);
+    // Destructor.
+    ~BezierCurve();
+
+    // Object changing methods.
+    // Define the bezier-curve.
+    // Sets the path.
+    void setPath(nav_msgs::Path path);
+    // Set the number of control points of the bezier-curve.
+    void setNumbCtrlPoints(int numb_ctr_points);
+    // Set the index of the point (in path_) around which the bezier-curve should be located.
+    void setCurrentArrayIndex(int curr_index);
+
+    // Object using methods.
+    // Do stuff with the previously defined bezier-curve.
+    // Find and set the nearest t;
+    void findT();
+    // For a defined bezier-curve, find the nearest t and the use calcCurvature(float t). Will be useful for pure-pursuit.
+    void calcCurvature();
+    // For a defined bezier-curve and a given parameter t, calculate and set the curvature.
+    void calcCurvature(float t);
+    // For a given bezier-curve and a given t, calculate the first derivatives. Will be mainly used in calcCurvature.
+    void calcXyDot(float t);
+    // At the current nearest_t_, calculate and set the second derivatives w.r.t to parameter t. Will be mainly used in calcCurvature.
+    void calcXyDotDot(float t);
+    // Output the calculated curvature at the active t.
+    float getCurvature();
+
+  private:
+      // Variables.
+      // The path.
+      nav_msgs::Path path_;
+      // How many points should be used for the bezier curve?.
+      int numb_ctr_points_;
+      // Current array position.
+      int current_arrayposition_;
+      // Using the current_arrayposition find the nearest parameter t and set this t to active t.
+      float nearest_t_;
+      // Parameter t at which the following variables will be calculated and stored.
+      float active_t_;
+      // The first derivative of x w.r.t t at the active_t_.
+      float x_dot_;
+      // The second derivative of x w.r.t t at the active_t_.
+      float x_dot_dot_;
+      // The first derivative of y w.r.t t at the active_t_.
+      float y_dot_;
+      // The second derivative of y w.r.t t at the active_t_.
+      float y_dot_dot_;
+      // The curvature at the active_t_.
+      float curvature_;
+}
--- src/bezier.cpp
+++ src/bezier.cpp
@@ -1,1 +1,75 @@
 #include "../include/interpolation/bezier.hpp"
+
+// Default constructor.
+BezierCurve::BezierCurve()
+{
+  // Object created.
+}
+// Useful constructor for pure pursuit.
+BezierCurve::BezierCurve(nav_msgs::Path path, int numb_ctr_points)
+{
+  // Save the path from the argument.
+  this->BezierCurve::setPath(path);
+  // Set the number of control points.
+  this->BezierCurve::setNumbCtrlPoints(numb_ctr_points);
+  std::cout<<"Bezier Curve Object created"<< std::endl;
+}
+BezierCurve::~BezierCurve()
+{
+  // Object Destroyed.
+}
+
+// Object changing methods.
+// Sets the path.
+void BezierCurve::setPath(nav_msgs::Path path)
+{
+  path_ = path;
+}
+// Set the number of control points of the bezier-curve.
+void BezierCurve::setNumbCtrlPoints(int numb_ctr_points)
+{
+  numb_ctr_points_ = numb_ctr_points;
+}
+void BezierCurve:.setCurrentArrayIndex(int curr_index)
+{
+    current_arrayposition_ = curr_index;
+}
+
+// Object using methods.
+// Find and set the nearest t;
+void BezierCurve::findT()
+{
+  // Iterate trough all points in a resolution of 0.1.
+  // Iterate trough all points in a resolution of 0.01.
+}
+void BezierCurve::calcCurvature()
+{
+  this->BezierCurve::findT();
+  *this.active_t_ = *this.nearest_t_;
+  this->calcCurvature(*this.active_t_);
+}
+void BezierCurve::calcCurvature(float t)
+{
+  float curvature;
+  x_dot = *this.x_dot_;
+  x_dot_dot = *this.x_dot_dot_;
+  y_dot = *this.y_dot_;
+  y_dot_dot = *this.y_dot_dot_;
+  this->BezierCurve::calcXyDot(t);
+  this->BezierCurve::calcXyDotDot(t);
+  curvature = (x_dot*y_dot_dot - x_dot_dot*y_dot)/pow(pow(x_dot,2) + pow(y_dot,2),1.5);
+  *this.curvature_ = curvature;
+}
+void BezierCurve::calcXyDot(float t)
+{
+
+
+}
+void BezierCurve::calcXyDotDot(float t)
+{
+
+}
+float BezierCurve::getCurvature
+{
+  return *this.curvature_;
+}
